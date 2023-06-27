e91c6d9c7b3fb62f5d162a2d2d8b99398847aa0e
Param Dave <param.dave@epita.fr>
2023-06-27 17:31:25 +0200

    feat(drone): serialize report

--- drone_program/src/main/scala/KafkaProducerDrone.scala
+++ drone_program/src/main/scala/KafkaProducerDrone.scala
@@ -1,11 +1,16 @@
 import java.util.Properties
 import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord}
 
+import io.circe._
+import io.circe.generic.auto._
+import io.circe.parser._
+import io.circe.syntax._
+import io.circe.syntax.EncoderOps
+
+
 object KafkaProducerDrone {
 
-  case class Report(id: Int, location: (Int, Int), citizens: List[String], score: List[Int], words: List[String])
-
-  case class Dummy(s1: String, s2: String, s3: String)
+  case class Report(id: Int, location: (Double, Double), citizens: List[String], score: List[Double], words: List[String])
 
   def main(args: Array[String]): Unit = {
     // Print hello
@@ -36,10 +41,10 @@ object KafkaProducerDrone {
 
     val topic = "drone-message"
 
-    val obj = Dummy("Hello", "World", "Scala")
+    val obj = Report(69, (42.12, 42.23), List("Pierre", "Hugo", "Param", "DarkSasuke"), List(75.3, 75.3, 50.4, 6.9), List("love", "peace", "happy", "hate"))
     val json: Json = obj.asJson
 
-    val record = new ProducerRecord[String, String](topic, "1", json)
+    val record = new ProducerRecord[String, String](topic, "1", json.toString)
 
     producer.send(record)
 
