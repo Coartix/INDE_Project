65a683a4f8758d00fd17280855eac8eb7944902e
Param Dave <param.dave@epita.fr>
2023-06-27 16:45:53 +0200

    feat(drone): add report class

--- drone_program/src/main/scala/KafkaProducerDrone.scala
+++ drone_program/src/main/scala/KafkaProducerDrone.scala
@@ -2,6 +2,9 @@ import java.util.Properties
 import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord}
 
 object KafkaProducerDrone {
+
+  case class Report(id: Int, location: (Int, Int), citizens: List[String], score: List[Int], words: List[String])
+
   def main(args: Array[String]): Unit = {
     // Print hello
     println("Hello, world!")
@@ -20,14 +23,19 @@ object KafkaProducerDrone {
 
     val producer = new KafkaProducer[String, String](props)
 
-    val position = "latitude: 37.7749, longitude: -122.4194"
+    /* val position = "latitude: 37.7749, longitude: -122.4194"
     val number = 42
 
     val record = new ProducerRecord[String, String](
       "drone-message",
       position,
       number.toString
-    )
+    )*/
+
+    val topic = "drone-message"
+
+    val record = new ProducerRecord[String, String](topic, i.toString, "My Site is sparkbyexamples.com " + i)
+
     producer.send(record)
 
     producer.close()
